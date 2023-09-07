e3fe2a809df7bed298bb7571676d6e30f94b0d63
Anton Tishchenko <AntonTishchenk0@yandex.ru>
2023-09-07 03:53:48 +0200

    Start Python

---
+++ lectures1/lesson1.py
@@ -0,0 +1,81 @@
+# n = 5
+# print(type(n))
+"""
+n = "sdasf0"
+print(type(n))
+"""
+# n = "asda\"dsfsf"
+# print(n)
+
+# a = 5
+# b = 5.234
+# c = 'sdfa'
+
+# print(f"{a} - {b} - {c}")
+# print("{} - {} - {}".format(a,b,c))
+
+# print("Введите 1ю строку")
+# a = input()
+# print(a)
+
+# print("Введите первое число: ")
+# a = input()
+# #print(a)
+# b = input("Введите второе число: ")
+# #print(b)
+
+# print(a, ' + ', b, ' = ', a+b)
+
+# c = 5.89
+# print(c)
+# n = int(c)
+# print(n)
+
+# c = 1
+# c = bool(c)
+# print(c)
+
+# print("Введите первое число: ")
+# a = int(input())
+# b = int(input("Введите второе число: "))
+
+
+# print(a, ' + ', b, ' = ', a+b)
+
+# a = 5.123423553
+# b = 6.678987653
+# print(round(a*b, 2))
+
+# iter = 2 # iter = iter + 2
+# iter += 3 # iter = iter + 3 
+# iter -= 4 # iter = iter - 4
+# iter *= 5 # iter = iter * 5
+# iter /= 5 # iter = iter / 5
+# iter //= 5 # iter = iter // 5
+# iter %= 5 # iter = iter % 5
+# iter **= 5 # iter = iter ** 5
+
+# a = 1 > 4
+# print(a)
+# a = 1 < 4 and 5 > 2
+# print(a)
+# a = 1 == 2
+# print(a)
+# a = 1 != 2
+# print(a)
+# a = 'qwe'
+# b = 'qwe'
+# print(a == b)
+# a = 1 < 3 < 5 < 10
+# print(a)
+
+# username = input('Input name: ')
+# if username == 'Masha':
+#     print('Ура, это же Masha!')
+# elif username == 'Марина':
+#     print('Я так ждала вас, Марина!')
+# elif username == 'Ильнар':
+#     print('Ильнар - топ!')
+# else:
+#     print('Привет, ', username)
+
