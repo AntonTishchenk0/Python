eec617462dbce183162b45934010a20147bf67d2
Anton Tishchenko <AntonTishchenk0@yandex.ru>
2023-09-22 04:23:29 +0200

    finish HW3

--- HomeWork3/HW3.py
+++ HomeWork3/HW3.py
@@ -64,3 +64,14 @@
 #  Ввод: ноутбук
 #  Вывод: 12
 
+# scrabble = {1: "A, E, I, O, U, L, N, S, T, R, А, В, Е, И, Н, О, Р, С, Т", 2: "D, G, Д, К, Л, М, П, У",
+#             3: "B, C, M, P, Б, Г, Ё, Ь, Я", 4: "F, H, V, W, Y, Й, Ы", 5: "K, Ж, З, Х, Ц, Ч",
+#             8: "J, X, Ш, Э, Ю", 10: "Q, Z, Ф, Щ, Ъ"}
+# word = str(input("Input a word: ").upper())
+# count = 0
+# for char in word:
+#     for key in scrabble:
+#         if char in list(scrabble[key]):
+#             count += key
+# print(count)
+        
